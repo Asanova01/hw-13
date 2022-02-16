3dfca73ef9139cc2e9036821ddae4edc5bc9fa2d
ulukmyrza2001 <97427570+ulukmyrza2001@users.noreply.github.com>
2022-02-16 21:50:53 +0600

    hw13

--- 13/src/components/UI/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Card.module.css'
-
-const Card = (props) => {
-  return (
-    <div className={classes.signupSection}>{props.children}</div>
-  )
-}
-export default Card
--- 13/src/helpers/regex.js
+++
@@ -1,5 +0,0 @@
-export const validEmailRegex = RegExp(
-	/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-	);
-export const validateUserName = RegExp(/[0-9]/)
-export let validatPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
